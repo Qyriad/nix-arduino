c3861187d0342789ab68ec74b7542f92fb130e97
Qyriad <qyriad@qyriad.me>
2023-09-16 18:20:50 -0600

    working example for adafruit samd and all its dependencies
    Sadly dependencies are manually managed for now

--- flake.nix
+++ flake.nix
@@ -18,13 +18,6 @@
 
         attrsToList = attrs: lib.attrsets.mapAttrsToList (name: value: value) attrs;
 
-        checksumKinds = [
-          ({ prefix = "SHA-256:"; kind = "sha256"; })
-          ({ prefix = "MD5:"; kind = "md5"; })
-        ];
-
-        # HACK: some things are still specified with MD5, so this set here will indicate what
-        # MD5 hashes to replace, which will need to be manually updated.
         systemDoubleFromString = string:
           let
             inherit (pkgs.lib.systems.parse) mkSystemFromString doubleFromSystem;
@@ -140,7 +133,46 @@
                     (tool: lib.attrsets.nameValuePair tool.name tool)
                 );
 
-                mkPlatform = { platformName, version, url, checksum, architecture }:
+                mkTool = { toolName, version, url, checksum, host }:
+                  let
+                    outPath = "${packageName}/tools/${toolName}/${version}";
+                  in
+                    pkgs.stdenv.mkDerivation {
+                      pname = "arduino-${packageName}-${toolName}";
+                      inherit version;
+
+                      src = fetchurl {
+                        inherit url;
+                        hash = mkHash checksum;
+                      };
+
+                      # HACK: fetchurl doesn't know how to unpack zip files, but
+                      # fetchzip hashes *after* unpack. Also, some of these archives have
+                      # a root folder and some don't. Let's just use aunpack to normalize it all.
+                      unpackCmd = ''
+                        export PATH="$PATH:${aunpack}/bin"
+                        aunpack $src
+                      '';
+                      dontConfigure = true;
+                      dontBuild = true;
+
+                      installPhase = ''
+                        mkdir -p "$out/${outPath}"
+                        cp -r ./* "$out/${outPath}/"
+                      '';
+                    }
+                ; # mkTool
+
+                tools = lib.attrsets.mapAttrs
+                  (name: value: mkTool {
+                    toolName = name;
+                    inherit (value) version url checksum host;
+                  })
+                  toolsByName
+                ;
+
+                # TODO: toolDependencies not yet implemented.
+                mkPlatform = { platformName, version, url, checksum, architecture, platformAttrs ? {} }:
                   let
                     outPath = "${packageName}/hardware/${architecture}/${version}";
                     # FIXME: comment
@@ -169,36 +201,9 @@
                         mkdir -p "$out/${outPath}"
                         cp -r ./* "$out/${outPath}/"
                       '';
+
+                      passthru.platformAttrs = platformAttrs;
                     }
-                ;
-
-                mkTool = { toolName, version, url, checksum, host }:
-                  let outPath = "${packageName}/tools/${toolName}/${version}";
-                in
-                  pkgs.stdenv.mkDerivation {
-                    pname = "arduino-${packageName}-${toolName}";
-                    inherit version;
-
-                    src = fetchurl {
-                      inherit url;
-                      hash = mkHash checksum;
-                    };
-
-                    # HACK: fetchurl doesn't know how to unpack zip files, but
-                    # fetchzip hashes *after* unpack. Also, some of these archives have
-                    # a root folder and some don't. Let's just use aunpack to normalize it all.
-                    unpackCmd = ''
-                      export PATH="$PATH:${aunpack}/bin"
-                      aunpack $src
-                    '';
-                    dontConfigure = true;
-                    dontBuild = true;
-
-                    installPhase = ''
-                      mkdir -p "$out/${outPath}"
-                      cp -r ./* "$out/${outPath}/"
-                    '';
-                  }
                 ;
 
               in {
@@ -207,17 +212,12 @@
                   (name: value: mkPlatform {
                     platformName = name;
                     inherit (value) version url checksum architecture;
+                    platformAttrs = value;
                   })
                   platformsByArch
                 ;
 
-                tools = lib.attrsets.mapAttrs
-                  (name: value: mkTool {
-                    toolName = name;
-                    inherit (value) version url checksum host;
-                  })
-                  toolsByName
-                ;
+                inherit tools;
               }
             ; # mkPackage
 
@@ -254,46 +254,65 @@
 
             attrs = builtins.fromJSON ((builtins.readFile "${drv}/index.json"));
 
-          in {
-            inherit drv attrs;
             packages = mkIndexPackages attrs;
+
+          in {
+            inherit drv attrs packages;
+
+            allPlatforms =
+              lib.lists.flatten (
+                lib.lists.forEach
+                  (attrsToList packages)
+                  (package: attrsToList package.platforms)
+              )
+            ;
+
+            allTools =
+              lib.lists.flatten (
+                lib.lists.forEach
+                  (attrsToList packages)
+                  (package: attrsToList package.tools)
+              )
+            ;
           }
+
         ; # fetchFromArduinoIndex
 
 
-        arduino = fetchArduinoIndex {
+        arduinoIndex = fetchArduinoIndex {
           url = "https://downloads.arduino.cc/packages/package_index.json";
           sha256 = "2793dbf068ec2d3b86c060e42a8f5a2faa6a61c799481bb1b9ff20989cbd938e";
         };
 
-        adafruit = fetchArduinoIndex {
+        adafruitIndex = fetchArduinoIndex {
           url = "https://adafruit.github.io/arduino-board-index/package_adafruit_index.json";
           sha256 = "0dsxql1lw6c6yhdsmdynvqs342vxlc0h553xbcdc4zjhknv7p5jh";
         };
 
-        adafruitPlatforms = pkgs.symlinkJoin {
-          name = "adafruit";
-          paths = (lib.attrsets.mapAttrsToList (name: value: value) adafruit.packages.adafruit.platforms) ++ (lib.attrsets.mapAttrsToList (name: value: value) adafruit.packages.adafruit.tools);
-        };
-
-        arduinoPackages = pkgs.symlinkJoin {
-          name = "arduinoPackages";
-          paths = (attrsToList arduino.packages.arduino.platforms) ++ (attrsToList arduino.packages.arduino.tools) ++ (attrsToList arduino.packages.builtin.tools);
-        };
-
-        both = pkgs.symlinkJoin {
-          name = "arduino-multi";
-          paths = [
-            adafruitPlatforms
-            arduinoPackages
-          ];
+        adafruitSamdPkgs = pkgs.symlinkJoin {
+          name = "adafruitSamdPkgs";
+          paths =
+            let
+              inherit (adafruitIndex.packages) adafruit;
+              inherit (arduinoIndex.packages) arduino;
+            in [
+              adafruit.platforms.samd
+              # Dependencies for adafruit.packages.adafruit.platforms.samd.
+              adafruit.tools.arm-none-eabi-gcc
+              adafruit.tools.bossac
+              arduino.tools.bossac
+              arduino.tools.openocd
+              adafruit.tools."CMSIS"
+              adafruit.tools."CMSIS-Atmel"
+              arduino.tools."arduinoOTA"
+            ];
         };
 
       in {
-        packages.default = both;
-        platforms = {
-          inherit adafruit;
-          inherit arduino;
+        packages.default = adafruitSamdPkgs;
+        indexes = {
+          inherit adafruitIndex;
+          inherit arduinoIndex;
         };
       }
     )
